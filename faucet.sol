f92fc9ba4516c1dd6de586a566e737bf8ab564d3
Taylor Anderson <taylor01anderson@icloud.com>
2025-07-24 20:36:14 -0700

    first solidity contract

---
+++ faucet.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0  
+
+// A contract that withdraws and accepts ether
+contract faucet {
+    // 'give ether to anyone who asks
+    function winthdraw(uint withdraw_amount) public {
+
+        // limit withdrawal amount to 0.1 ether
+        require(withdraw_amount <= 100000000000000000, "You can withdraw a maximum of 0.1 ether");
+
+        // send the requested amount to the caller
+        payable(msg.sender).transfer(withdraw_amount);
+    }
+
+    // fallback function to accept incoming ether
+    receive() external payable {}
+}
